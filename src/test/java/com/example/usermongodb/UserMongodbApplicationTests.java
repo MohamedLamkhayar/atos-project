15804dd683533f8bd17f0a599a2a0952813f7372
mohamedLamkhayar <lamkhayarmohamed2@gmail.com>
2021-07-18 13:10:10 +0200

    first commit

---
+++ src/test/java/com/example/usermongodb/UserMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usermongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
